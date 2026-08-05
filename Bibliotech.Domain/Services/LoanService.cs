5e1e0441d34c6e5f7d9f7d266d56afab30999e8a
Aymen ABEDA <aymenabeda@outlook.com>
2026-08-05 20:00:12 +0100

    feat: add services

---
+++ Bibliotech.Domain/Services/LoanService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotech.Domain.Services
+{
+    internal class LoanService
+    {
+    }
+}

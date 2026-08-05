10ad8d6769f4a414eeb9828d5dcca6279a2cf8ae
Aymen ABEDA <aymenabeda@outlook.com>
2026-08-05 20:49:00 +0100

    fix: update member repository

--- Bibliotech.Infrastructure/Repositories/MemberRepository.cs
+++ Bibliotech.Infrastructure/Repositories/MemberRepository.cs
@@ -1,12 +1,32 @@
 ﻿using Bibliotech.Domain.Entities;
+using Bibliotech.Domain.Repositories;
+using Bibliotech.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace Bibliotech.Domain.Repositories
+namespace Bibliotech.Infrastructure.Repositories
 {
-    public interface IMemberRepository
+    public class MemberRepository : IMemberRepository
     {
-        Task<Member?> GetByIdAsync(int id);
-        Task<IEnumerable<Member>> GetAllAsync();
-        Task AddAsync(Member member);
-        Task SaveChangesAsync();
+        private readonly BibliotechDbContext _context;
+
+        public MemberRepository(BibliotechDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Member?> GetByIdAsync(int id) =>
+            //_context.Members.FirstOrDefaultAsync(m => m.Id == id);
+            _context.Members.FindAsync(id).AsTask();
+
+        public Task<IEnumerable<Member>> GetAllAsync() =>
+            Task.FromResult(_context.Members.AsEnumerable());
+
+        public async Task AddAsync(Member member)
+        {
+            await _context.Members.AddAsync(member);
+        }
+
+        public Task SaveChangesAsync() =>
+            _context.SaveChangesAsync();
     }
 }

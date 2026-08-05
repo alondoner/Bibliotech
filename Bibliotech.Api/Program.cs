f1a5a1fa502b3a7c061cb2a49dac1d2be51f15b1
Aymen ABEDA <aymenabeda@outlook.com>
2026-08-05 20:53:06 +0100

    feat: update main program

--- Bibliotech.Api/Program.cs
+++ Bibliotech.Api/Program.cs
@@ -1,13 +1,52 @@
+using Bibliotech.Domain.Services;
+using Bibliotech.Domain.Repositories;
+using Bibliotech.Infrastructure.Data;
+using Bibliotech.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- Database (SQLite) ---
+builder.Services.AddDbContext<BibliotechDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
+// --- Repositories ---
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<ILoanRepository, LoanRepository>();
+
+// --- Domain Services ---
+builder.Services.AddScoped<BookService>();
+builder.Services.AddScoped<MemberService>();
+builder.Services.AddScoped<LoanService>();
+
+// --- Controllers ---
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+// Create database and tables
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BibliotechDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +56,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ReactPolicy");
 
 app.UseAuthorization();
 
